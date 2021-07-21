95169372776ef13b06050ab8fc6e78f7c323a6b5
dabramov <dzmitry.abramau@gmail.com>
2021-07-21 23:59:46 +0300

    add version print

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include "config.h"
+#include "version.h"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+    Serial.begin(CONFIG_UART_BAUD_RATE);
+    Serial.setDebugOutput(true);
+
+    printf("\n\n");
+    printf("========================\n");
+    if (BUILD_VERSION < 0)
+    {
+        printf("  LOCAL BUILD: %03d\n", - BUILD_VERSION);
+    }
+    else
+    {
+        printf("  FW REV: %03d\n", BUILD_VERSION);
+    }
+    printf("  %s %s\n", __DATE__, __TIME__);
+    printf("========================\n");
+    printf("\n");
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
 }
